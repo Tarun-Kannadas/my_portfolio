b9b95d62029e330dadaf1cf234ec82a19b8e5581
Tarun Kannadas <tarunkannadas@gmail.com>
2025-09-18 12:56:45 +0530

    converted internal js to external js

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function driveLink()
+{
+    window.open('https://drive.google.com/file/d/1V78AOaJEb0miocgBu_q5oKyfUSb1tMXg/view?usp=drive_link','_blank')
+}
+
+function linkedIn()
+{
+    window.open('https://www.linkedin.com/in/tarun-kannadas-69ba93240/','_blank')
+}
+
+function behance()
+{
+    window.open('https://www.behance.net/tarunkannadas','_blank')
+}
+
+function github()
+{
+    window.open('https://github.com/Tarun-Kannadas','_blank')
+}
+
+function jobPortal()
+{
+    window.open('https://job-portal-bnre.onrender.com','_blank')
+}
+
+function neuraloop()
+{
+    window.open('https://www.neuraloopai.com/home','_blank')
+}
+
+function vethnix()
+{
+    window.open('https://www.vethnix.com','_blank')
+}
